a7793e58cdb55cb08f47b2ae75dbe497a66013f5
Damir9713 <damir12850@hotmail.fr>
2021-09-25 03:11:51 +0200

    first upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typed", {
+  strings: ["DEV FRONT END", "DEV BACK END"],
+  smartBackspace: true, // Default value
+  typeSpeed: 70,
+  loop: true,
+  loopCount: Infinity,
+});
